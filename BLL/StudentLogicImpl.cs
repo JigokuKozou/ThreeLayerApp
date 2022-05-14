5cf81a0d2ec54645bd9bfcbc696251650f7f5ac3
UltimateEscanor <ilyashchelkin@mail.ru>
2022-05-14 13:44:19 +0400

    Added student logic implementation

--- BLL/StudentLogicImpl.cs
+++ BLL/StudentLogicImpl.cs
@@ -1,21 +1,35 @@
-﻿using ThreeLayerApp.Entities;
+﻿using System.Collections.Generic;
+using System.Linq;
+using ThreeLayerApp.DAL;
+using ThreeLayerApp.Entities;
 
 namespace ThreeLayerApp.BLL
 {
     public class StudentLogicImpl : IStudentLogic
     {
+        IRepo<Student> _repository;
+
+        public StudentLogicImpl(IRepo<Student> repository)
+        {
+            _repository = repository;
+        }
+
         public Student Create(string name, int age, float weigth, float height,
             int yearStartOfStudy, int course, int group)
-            => new Student(name, age, weigth, height, yearStartOfStudy, course, group);
-
-        public bool TryDelete(int index)
         {
-            throw new System.NotImplementedException();
+            var student = new Student(name, age, weigth, height, yearStartOfStudy, course, group);
+
+            return _repository.Add(student);
         }
 
-        public Student Update(int index, string name, int age, float weigth, float height, int yearStartOfStudy, int course, int group)
-        {
-            throw new System.NotImplementedException();
-        }
+        public Student Update(int index, string name, int age, float weigth, float height, 
+            int yearStartOfStudy, int course, int group)
+            => _repository.Update(index, new Student(name, age, weigth, height, yearStartOfStudy, course, group));
+
+        public Student Find(int index) => _repository.GetAll().Skip(index).First();
+
+        public IEnumerable<Student> FindAll() => _repository.GetAll();
+
+        public bool TryDelete(int index) => _repository.TryDelete(index);
     }
 }
